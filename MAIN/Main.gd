807716ec301f2cedca351de4f5fbb2551aab8c65
Antoine <antoinesaget19@gmail.com>
2018-08-11 17:35:25 +0200

    ...

--- MAIN/Main.gd
+++ MAIN/Main.gd
@@ -9,13 +9,8 @@ var production = 0
 var stock = 100
 
 func _ready():
-	
 	init_grid()
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 func _process(delta):
 	print(production)
 func init_grid():
@@ -33,11 +28,11 @@ func _input(event):
 func initLab():
 	var node = labScene.instance()
 	add_child(node)
+	
 func initMine():
 	var node = mineScene.instance()
 	add_child(node)
 	
-
 func isBuildable(var building):
 	if building.pos.x < 0 or building.pos.x + building.size.x > 100\
 	or building.pos.y < 0 or building.pos.y + building.size.y > 100:
@@ -54,14 +49,8 @@ func isBuildable(var building):
 			underMe += 1 
 	if underMe == 0:
 		return false
-	
 	return true
 
-#func _on_Timer_timeout():
-#	ressources += production
-#	print(production)
-#	$CanvasLayer/UI/BatimentsContainrer/RessourcesContainer/Minerai.text = str("Minerai : \n", ressources, " / ", stock)
-		
 func add_building_grid(building):
 	for x in range(building.pos.x, building.pos.x + building.size.x):
 		for y in range(building.pos.y, building.pos.y + building.size.y):
