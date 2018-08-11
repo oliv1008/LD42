550d6ff89c93afe3557eca322da5ebddaf67a872
Antoine <antoinesaget19@gmail.com>
2018-08-11 18:49:34 +0200

    Gestion des ressources, hauteur maximum de construction, entrepots,...

--- MAIN/Main.gd
+++ MAIN/Main.gd
@@ -1,62 +1,9 @@
 extends Node2D
 
-var Grid = []
-var labScene = preload("res://BATIMENT/Laboratoire.tscn")
-var mineScene = preload("res://BATIMENT/Mine.tscn")
-
-var ressources = 0
-var production = 0
-var stock = 100
+func _on_Timer_timeout():
+	Global.ressources += Global.production
+	$CanvasLayer/UI/BatimentsContainrer/RessourcesContainer/Minerai.text = str("Ressources :\n", Global.ressources, " / ", Global.stock)
 
 func _ready():
-	Global.init(self)
-	init_grid()
-
-func _process(delta):
-	print(production)
-func init_grid():
-	for x in range(0,100):
-		Grid.append([])
-		for y in range(0,100):
-			Grid[x].append(null)
-
-func _input(event):
-	if event.is_action_pressed("Laboratoire"):
-		initLab()
-	elif event.is_action_pressed("Mine"):
-		initMine()
-
-func initLab():
-	var node = labScene.instance()
-	add_child(node)
-	
-func initMine():
-	var node = mineScene.instance()
-	add_child(node)
-	
-func isBuildable(var building):
-	if building.pos.x < 0 or building.pos.x + building.size.x > 100\
-	or building.pos.y < 0 or building.pos.y + building.size.y > 100:
-		return false
-	
-	for x in range(building.pos.x, building.pos.x + building.size.x):
-		for y in range(building.pos.y, building.pos.y + building.size.y):
-			if Grid[x][y] != null:
-				return false
-	
-	var underMe = 0
-	for x in range(building.pos.x, building.pos.x + building.size.x):
-		if Grid[x][building.pos.y - 1] != null or building.pos.y == 0:
-			underMe += 1 
-	if underMe == 0:
-		return false
-	return true
-
-func add_building_grid(building):
-	for x in range(building.pos.x, building.pos.x + building.size.x):
-		for y in range(building.pos.y, building.pos.y + building.size.y):
-			Grid[x][y] = building
-
-func addProduction(add):
-	production += add
-	print(production)
+	$Line2D.set_point_position(0, Vector2(0, -64 * Global.hauteurMaxDeConstruction))
+	$Line2D.set_point_position(1, Vector2(64*100, -64 * Global.hauteurMaxDeConstruction))
