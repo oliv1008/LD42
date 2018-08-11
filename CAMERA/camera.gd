9f02cebe7c7d44e654d51d304295932b98e506c9
Antoine <antoinesaget19@gmail.com>
2018-08-11 14:06:51 +0200

    Camera

--- CAMERA/camera.gd
+++ CAMERA/camera.gd
@@ -12,13 +12,24 @@ func _ready():
 	pass
 
 func _process(delta):
-	var direction = Vector2()
-	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	direction = direction.normalized()
-	
-	var velocity = speed * direction * delta
-	position = position + velocity
+#	var direction = Vector2()
+#	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+#	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+#	direction = direction.normalized()
+#
+#	var velocity = speed * direction * delta
+#	position = position + velocity
+	pass
 
 func _input(event):
-	pass
+	if event.is_action_pressed("CameraSlide") :
+#		var newTime = OS.get_ticks_msec()
+#		var newPos = get_global_mouse_position()
+#		var delta = newTime - oldTime
+#		position += newPos - oldPos
+#		oldTime = newTime
+#		oldPos = newPos
+#	else :
+#		oldTime = OS.get_ticks_msec()
+#		oldPos = get_global_mouse_position()
+		
