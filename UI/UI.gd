fc7720055023713ad8e5a7df98b5739367ef01e8
oliv1008 <omillochau@gmail.com>
2018-08-12 20:52:26 +0200

    Recherches

--- UI/UI.gd
+++ UI/UI.gd
@@ -1,5 +1,10 @@
 extends Control
 
+var TextureResearchIcon = { "Sky_is" : preload("res://Assets/Pixel Art/Icones/icone augmente limite tech.png"),\
+							"Pisa" : preload("res://Assets/Pixel Art/Icones/icone leaning tower of pisa tech.png"),\
+							"Rocket" : preload("res://Assets/Pixel Art/Icones/icone tech rocket.png")
+}
+
 func _on_Laboratory_pressed():
 	Global.initScene(Global.labScene)
 
@@ -26,3 +31,35 @@ func _on_ResearchButton_pressed():
 		$ResearchContainer.visible = true
 	else:
 		$ResearchContainer.visible = false
+
+func _on_Upgrade_Limit_pressed():
+#On check d'abord pour voir si le joueur peut lancer la recherche	
+#A IMPLEMENTER
+	$IconResearch.texture = TextureResearchIcon.Sky_is
+	$IconResearch.visible = true
+	$ResearchContainer.visible = false
+	$GlobalTimer.wait_time = 10
+	$GlobalTimer.start()
+	$ResearchAndLoadingContainer/LifeBar.visible = true
+	$ResearchAndLoadingContainer/ResearchButton.visible = false
+	$ResearchAndLoadingContainer/LifeBar.update(0, 100)
+	$SecondTimer.start()
+
+func _on_Pisa_pressed():
+	pass # replace with function body
+
+
+func _on_Rocket_Research_pressed():
+	pass # replace with function body
+	
+func update_loading_bar():
+	var time_passed = $GlobalTimer.wait_time - $GlobalTimer.time_left
+	var health = time_passed * 100 / $GlobalTimer.wait_time
+	$ResearchAndLoadingContainer/LifeBar.update(health, 100)
+	
+func on_research_over():
+	$GlobalTimer.stop()
+	$SecondTimer.stop()
+	$IconResearch.visible = false
+	$ResearchAndLoadingContainer/LifeBar.visible = false
+	$ResearchAndLoadingContainer/ResearchButton.visible = true
